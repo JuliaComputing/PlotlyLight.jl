82ce9d9b8dc821d84978e40a32d469c773f890fe
joshday <emailjoshday@gmail.com>
2020-03-25 11:53:07 -0400

    first commit

--- src/PlotlyLight.jl
+++ src/PlotlyLight.jl
@@ -1,5 +1,122 @@
 module PlotlyLight
 
-greet() = print("Hello World!")
+using Random
+using JSON3
+using OrderedCollections
+
+const plotlyjspath = joinpath(@__DIR__(), "..", "deps", "plotly-latest.min.js")
+
+#-----------------------------------------------------------------------------# plot 
+function plot(data=[trace()], lay=layout(), conf=config(), dest=joinpath(tempdir(), "plotlylight.html"))
+    write_html(data, lay; conf=conf, dest=dest)
+end
+
+#-----------------------------------------------------------------------------# utils
+function filldict!(d, kw)
+    for (k, v) in kw 
+        d[k] = v
+    end
+    return d
+end
+
+const dict = OrderedDict{Symbol, Any}
+
+#-----------------------------------------------------------------------------# trace
+"""
+    trace(; kw...)
+"""
+trace(; kw...) = filldict!(dict(:x => nothing, :y => nothing, :type => nothing), kw)
+
+#-----------------------------------------------------------------------------# layout
+"""
+    layout(; kw...)
+
+Create the layout dict.  List of options is available at:
+
+https://plotly.com/javascript/reference/#layout
+"""
+layout(; kw...) = filldict!(dict(:title => "PlotlyLight Plot"), kw)
+
+#-----------------------------------------------------------------------------# config
+"""
+    config(; kw...)
+
+Create the configuration dict.  List of options is available at:
+
+https://github.com/plotly/plotly.js/blob/master/src/plot_api/plot_config.js#L22-L86
+"""
+function config(; kw...)
+    d = OrderedDict{Symbol,Any}(
+        :staticPlot                 => false,
+        :editable                   => false,
+        :autosizable                => true,
+        :queueLength                => 0,
+        :fillFrame                  => false,
+        :frameMargins               => 0,
+        :scrollZoom                 => false,
+        :showTips                   => true,
+        :showAxisDragHandles        => true,
+        :showAxisRangeEntryBoxes    => true,
+        :showLink                   => false,
+        :sendData                   => true,
+        :linkText                   => "Edit chart",
+        :showSources                => false,
+        :displayModeBar             => true,
+        :displaylogo                => false,
+        :responsive                 => true
+    )
+    filldict!(d, kw)
+end
+
+#-----------------------------------------------------------------------------# html_string
+function div_string(data = [trace()], lay = layout(); id=randstring(10), conf=config(), kw...)
+    data2 = data isa AbstractVector ? data : [data]
+    """
+    <div id=$(JSON3.write(id)))>
+    <script>
+        var data = $(JSON3.write(data2))
+        var layout = $(JSON3.write(lay))
+        Plotly.newPlot($(JSON3.write(id)), data, layout, $(JSON3.write(conf)))
+    </script>
+    """
+end
+
+#-----------------------------------------------------------------------------# html_string
+function html_string(divs::String...)
+    s = """
+    <!DOCTYPE html>
+    <html>
+    <head>
+    <title>PlotlyLight Visualization</title>
+    <script src = "$plotlyjspath"></script>
+    </head>
+    <body>
+    """
+    for div in divs 
+        s *= div 
+    end
+    s *= """
+    </body>
+    </html>
+    """ 
+    s
+end
+
+#-----------------------------------------------------------------------------# write_html
+function write_html(args...; dest=joinpath(tempdir(), "plot.html"), kw...)
+    touch(dest)
+    write(dest, html_string(div_string(args...; kw...)))
+    if Sys.iswindows()
+        run(`start $dest`)
+    elseif Sys.islinux()
+        run(`xdg-open $dest`)
+    elseif Sys.isapple()
+        Sys.run(`open $dest`)
+    else
+        @warn("Couldn't open $dest on system: $(Sys.KERNEL)")
+    end
+end
+
+
 
 end # module
