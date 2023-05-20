aae3e206764c678761f2f369d90685f5a5127a73
Andreas Kröpelin <andreas-kroepelin@gmx.de>
2023-05-20 16:22:27 +0200

    Test that defaults are not changed

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,5 @@
 using PlotlyLight
+using PlotlyLight.Defaults
 using JSON3
 using Test
 
@@ -11,6 +12,18 @@ using Test
     @test isempty(only(p.data))
     p(Config(x=1:10,y=rand(10)))
     @test length(p.data) == 2
+end
+#-----------------------------------------------------------------------------# defaults
+@testset "defaults" begin
+    old_layout_default = copy(Defaults.layout[])
+    old_config_default = copy(Defaults.config[])
+    p = Plot()
+    p.layout.xaxis.showgrid = false
+    p.config.editable = true
+    # make sure that mutation of layout and config of one plot has no effect on
+    # global defaults
+    @test Defaults.layout[] == old_layout_default
+    @test Defaults.config[] == old_config_default
 end
 #-----------------------------------------------------------------------------# src
 @testset "src" begin
